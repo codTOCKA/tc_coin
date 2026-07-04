e48b9133a4756944b8f78b56f34dd9a771a8e523
codTOCKA <imanjd1883@gmail.com>
2026-07-04 07:18:05 +0330

    script.js
    Added power variable and updated score calculation.

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 let score = 0;
+let power = 1;
+
+// load saved data
+if (localStorage.getItem("tca_score")) {
+  score = parseInt(localStorage.getItem("tca_score"));
+}
+
+if (localStorage.getItem("tca_power")) {
+  power = parseInt(localStorage.getItem("tca_power"));
+}
+
+document.getElementById("score").innerText = score;
 
 function tapCoin() {
-  score += 1;
+  score += power;
   document.getElementById("score").innerText = score;
+
+  localStorage.setItem("tca_score", score);
+}
+
+// upgrade system (simple)
+function upgrade() {
+  if (score >= 100) {
+    score -= 100;
+    power += 1;
+
+    localStorage.setItem("tca_score", score);
+    localStorage.setItem("tca_power", power);
+
+    document.getElementById("score").innerText = score;
+    alert("Upgraded! Power: " + power);
+  } else {
+    alert("Not enough TCA");
+  }
 }
